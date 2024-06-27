004260fce418e216ec661e19420703be3cc0aac5
VHuyYY <biprodn123@gmail.com>
2024-06-27 21:26:09 +0700

    baithuchanh

---
+++ Product3.js
@@ -0,0 +1,24 @@
+class Product {
+  maso;
+  loaitietkiem;
+  hoten;
+  cmnd;
+  ngaymoso;
+  sotiengui;
+  constructor(
+    masoInput,
+    loaitietkiemInput,
+    hotenInput,
+    cmndInput,
+    ngaymosoInput,
+    sotienguiInput
+  ) {
+    this.maso = masoInput;
+    this.loaitietkiem = loaitietkiemInput;
+    this.hoten = hotenInput;
+    this.loaitietkiem = loaitietkiemInput;
+    this.cmnd = cmndInput;
+    this.ngaymoso = ngaymosoInput;
+    this.sotiengui = sotienguiInput;
+  }
+}
